e0c6b5f48401ef8b6b20cb6033a1304456f78171
QUSAD.prod <qusad.prod@gmail.com>
2025-04-25 20:54:05 +0300

    add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:qusad_prod_web/app.dart';
 
 Future<void> main() async {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/Inter/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/JetBrains_Mono/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/Manrope/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/Raleway/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  GoogleFonts.config.allowRuntimeFetching = false;
+
   runApp(const App());
 }
